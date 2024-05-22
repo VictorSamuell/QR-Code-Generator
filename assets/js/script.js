d43cae309959f4553f88b070ae042a9665a63479
VictorSamuell <victor.samuel1@aluno.ifsp.edu.br>
2024-05-21 21:35:55 -0300

    Atualizações no script e no css

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,16 +1,16 @@
-let imgBox= document.getElementById("imgBox");
 let qrCode = document.getElementById("qrCode");
 let TextoQR = document.getElementById("textoQR");
+let imgBox= document.getElementById("imgBox");
 
-console.log(imgBox);
 console.log(qrCode);
 console.log(TextoQR);
+console.log(imgBox);
 
 
 function gerarQR(){
 
-    
     qrCode.src = " https://api.qrserver.com/v1/create-qr-code/?size=150x150&data=" + textoQR.value;
+    document.getElementById('qrCode').classList.remove('hidden');
 
 
 };
