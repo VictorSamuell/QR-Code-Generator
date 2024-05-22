8568f4e8de12e9d40b6c237ffdd3363e8279ac97
VictorSamuell <victor.samuel1@aluno.ifsp.edu.br>
2024-05-21 21:27:39 -0300

    Resolução e Funcionamento do Gerador de QR code

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,16 @@
+let imgBox= document.getElementById("imgBox");
+let qrCode = document.getElementById("qrCode");
+let TextoQR = document.getElementById("textoQR");
 
-let imgBox = document.getElementById("imgBox");
-let qrCode = document.getElementById("qrCode")
+console.log(imgBox);
+console.log(qrCode);
+console.log(TextoQR);
+
+
+function gerarQR(){
+
+    
+    qrCode.src = " https://api.qrserver.com/v1/create-qr-code/?size=150x150&data=" + textoQR.value;
+
+
+};
